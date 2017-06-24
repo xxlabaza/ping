7bd33120e98f7c9b79abaa62e009561e13ca23e9
Artem Labazin <xxlabaza@gmail.com>
2017-06-25 02:29:32 +0300

    Add CLI JUnit tests

--- src/main/java/ru/xxlabaza/test/ping/cli/CommandLineParser.java
+++ src/main/java/ru/xxlabaza/test/ping/cli/CommandLineParser.java
@@ -1,12 +1,12 @@
-/**
+/*
  * Copyright 2017 Artem Labazin <xxlabaza@gmail.com>.
- * <p>
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -25,7 +25,7 @@ import java.io.File;
 import java.util.ResourceBundle;
 import lombok.val;
 import org.slf4j.LoggerFactory;
-import ru.xxlabaza.test.ping.localization.UTF8Control;;
+import ru.xxlabaza.test.ping.localization.UTF8Control;
 
 /**
  * Command line arguments parser utility class.
@@ -35,44 +35,32 @@ import ru.xxlabaza.test.ping.localization.UTF8Control;;
  */
 public final class CommandLineParser {
 
-    private static final ResourceBundle BUNDLE;
+    public static void parse (String[] args) throws ParameterException {
+        val bundle = ResourceBundle.getBundle("localization/options", new UTF8Control());
+        val programName = "java -jar " + getProgramName();
 
-    private static final String PROGRAM_NAME;
-
-    private static final String PROGRAM_DESCRIPTION;
-
-    private static final String ARGUMENTS_ERROR_PREFIX;
-
-    static {
-        BUNDLE = ResourceBundle.getBundle("localization/options", new UTF8Control());
-
-        PROGRAM_NAME = "java -jar " + getProgramName();
-
-        PROGRAM_DESCRIPTION = BUNDLE.getString("program.description");
-        ARGUMENTS_ERROR_PREFIX = BUNDLE.getString("program.argument.parse.error");
-    }
-
-    public static void parse (String[] args) {
         val commonOptions = new CommonOptions();
 
         val commander = JCommander.newBuilder()
-                .programName(PROGRAM_NAME)
+                .programName(programName)
                 .addObject(commonOptions)
-                .resourceBundle(BUNDLE)
+                .resourceBundle(bundle)
                 .build();
 
         try {
             commander.parse(args);
         } catch (ParameterException ex) {
-            System.err.format(ARGUMENTS_ERROR_PREFIX, ex.getMessage());
+            val argumentErrorPrefix = bundle.getString("program.argument.parse.error");
+            System.err.format(argumentErrorPrefix, ex.getMessage());
             commander.usage();
-            System.exit(1);
+            throw ex;
         }
 
         if (commonOptions.isHelp()) {
-            System.out.println(PROGRAM_DESCRIPTION);
+            val programDescription = bundle.getString("program.description");
+            System.out.println(programDescription);
             commander.usage();
-            System.exit(0);
+            return;
         }
 
         if (commonOptions.isDebug()) {
---
+++ src/test/java/ru/xxlabaza/test/ping/cli/CommandLineParserTest.java
@@ -0,0 +1,143 @@
+/*
+ * Copyright 2017 xxlabaza.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ru.xxlabaza.test.ping.cli;
+
+import static ch.qos.logback.classic.Level.DEBUG;
+import static ch.qos.logback.classic.Level.INFO;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.slf4j.Logger.ROOT_LOGGER_NAME;
+
+import ch.qos.logback.classic.Logger;
+import com.beust.jcommander.ParameterException;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Locale;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Artem Labazin <xxlabaza@gmail.com>
+ * @since 25.06.2017
+ */
+public class CommandLineParserTest {
+
+    private static final ByteArrayOutputStream OUT;
+
+    private static Locale defaultLocale;
+
+    static {
+        OUT = new ByteArrayOutputStream();
+    }
+
+    @BeforeClass
+    public static void beforeClass () {
+        defaultLocale = Locale.getDefault();
+    }
+
+    @Before
+    public void before () {
+        System.setOut(new PrintStream(OUT));
+    }
+
+    @After
+    public void after () {
+        System.setOut(null);
+
+        Logger root = (Logger) LoggerFactory.getLogger(ROOT_LOGGER_NAME);
+        root.setLevel(INFO);
+
+        Locale.setDefault(defaultLocale);
+    }
+
+    @Test
+    public void testLongHelpUsage () {
+        String args[] = { "--help" };
+        CommandLineParser.parse(args);
+
+        assertTrue(OUT.toString().contains("Usage:"));
+    }
+
+    @Test
+    public void testShortHelpUsage () {
+        String args[] = { "-h" };
+        CommandLineParser.parse(args);
+
+        assertTrue(OUT.toString().contains("Usage:"));
+    }
+
+    @Test
+    public void testLongDebugPrint () {
+        Logger root = (Logger) LoggerFactory.getLogger(ROOT_LOGGER_NAME);
+        assertEquals(INFO, root.getLevel());
+
+        String args[] = { "--debug" };
+        CommandLineParser.parse(args);
+
+        assertEquals(DEBUG, root.getLevel());
+    }
+
+    @Test
+    public void testShortDebugPrint () {
+        Logger root = (Logger) LoggerFactory.getLogger(ROOT_LOGGER_NAME);
+        assertEquals(INFO, root.getLevel());
+
+        String args[] = { "-d" };
+        CommandLineParser.parse(args);
+
+        assertEquals(DEBUG, root.getLevel());
+    }
+
+    @Test(expected = ParameterException.class)
+    public void testParseException () {
+        String args[] = { "-z" };
+        CommandLineParser.parse(args);
+    }
+
+    @Test
+    public void testEnglishLocalization () {
+        Locale.setDefault(new Locale("en", "US"));
+
+        String args[] = { "-h" };
+        CommandLineParser.parse(args);
+
+        assertTrue(OUT.toString().contains("Client-server program"));
+    }
+
+    @Test
+    public void testRussianLocalization () {
+        Locale.setDefault(new Locale("ru", "RU"));
+
+        String args[] = { "-h" };
+        CommandLineParser.parse(args);
+
+        assertTrue(OUT.toString().contains("Клиент-серверная программа"));
+    }
+
+    @Test
+    public void testDefaultLocalization () {
+        Locale.setDefault(new Locale("fr", "FR"));
+
+        String args[] = { "-h" };
+        CommandLineParser.parse(args);
+
+        assertTrue(OUT.toString().contains("Client-server program"));
+    }
+}
